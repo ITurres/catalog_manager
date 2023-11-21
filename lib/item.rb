b27620329b873ab5ac021bc1c07820d85460a4a9
demesameneshoa <demesameneshoa@gmail.com>
2023-11-21 18:10:27 +0300

    Added custom setter and applied methods added custom setter for label implemented canbearchived and archive methods

--- lib/item.rb
+++ lib/item.rb
@@ -23,4 +23,20 @@ class Item
     @author = author
     author.add_item(self)
   end
+
+  def add_label(label)
+    @label = label
+    label.add_item(self)
+  end
+
+  def can_be_archived?
+    published_date = Date.parse(@publish_date)
+    (Date.today - published_date).to_i > 3650 # 10 years in days
+  end
+
+  def move_to_archive
+    return unless can_be_archived?
+
+    @archived = true
+  end
 end
