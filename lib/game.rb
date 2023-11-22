f2f55c5f777d9f6a432548bb46cdd50b683c9311
ITurres <emanueliturres.co@gmail.com>
2023-11-22 12:54:28 -0300

    Feat: Enhance 'Game' class constructor to:
    - Accept:
        - A item object with the instantiation of: Genre, Author and Label.
        - super publish_date with default value of nil
        - last_played_at with default value of nil

--- lib/game.rb
+++ lib/game.rb
@@ -4,8 +4,11 @@ require_relative 'item'
 class Game < Item
   attr_reader :multiplayer, :last_played_at
 
-  def initialize(last_played_at, multiplayer)
-    super(last_played_at)
+  def initialize(item, published_date: nil,
+                 last_played_at: nil,
+                 multiplayer: false)
+
+    super(item.genre, item.author, item.label, published_date)
     @last_played_at = last_played_at
     @multiplayer = multiplayer
   end
@@ -14,9 +17,6 @@ class Game < Item
 
   def can_be_archived?
     return false unless super
-    # ! After super, we know that the parent's method returns true so 'date' its already older than 10 years,
-    # ! so at this point it is implied that @last_played_at is older than 2 years...
-    # ? do we need the below check?.
 
     return false if @last_played_at.nil? || @last_played_at !~ /\d{4}-\d{2}-\d{2}/
 
