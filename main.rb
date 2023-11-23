b6b5848772a20e73534c919929583e1391b9ea1f
ITurres <emanueliturres.co@gmail.com>
2023-11-22 22:10:34 -0300

    Refactor: 'main.rb' and Fix method send error:
    - Give keys to each OPTION menu title and methods.
    - Refactor 'display_menu' method to properly display just
      the menu title.
    - Introduce while loop to validate user input.
    - Remove unnecessary conditional statements at the end of
      'handle_option'  and access OPTIONS method.

--- main.rb
+++ main.rb
@@ -1,30 +1,37 @@
 require_relative 'lib/app'
 
 OPTIONS = {
-  1 => { 'List all books' => :list_all_books },
-  2 => { 'List all music albums' => :list_all_music_albums },
-  3 => { 'List of games' => :list_of_games },
-  4 => { 'List all genres' => :list_all_genres },
-  5 => { 'List all labels' => :list_all_labels },
-  6 => { 'List all authors' => :list_all_authors },
-  7 => { 'Add a book' => :add_a_book },
-  8 => { 'Add a music album' => :add_a_music_album },
-  9 => { 'Add a game' => :add_a_game },
-  10 => { 'Quit' => :quit }
+  1 => { name: 'List all books', method: :list_all_books },
+  2 => { name: 'List all music albums', method: :list_all_music_albums },
+  3 => { name: 'List of games', method: :list_of_games },
+  4 => { name: 'List all genres', method: :list_all_genres },
+  5 => { name: 'List all labels', method: :list_all_labels },
+  6 => { name: 'List all authors', method: :list_all_authors },
+  7 => { name: 'Add a book', method: :add_a_book },
+  8 => { name: 'Add a music album', method: :add_a_music_album },
+  9 => { name: 'Add a game', method: :add_a_game },
+  10 => { name: 'Quit', method: :quit }
 }.freeze
 
 class Main
   def initialize
     @mainmenu = self
-    # TODO: Implement the App class
+
     @app = App.new
   end
 
   def display_menu
     puts "\nPlease choose an option:"
-    OPTIONS.each { |key, value| puts "#{key} - #{value.to_s.tr('_', ' ')}" }
+    OPTIONS.each { |options_index, value| puts "#{options_index} - #{value[:name]}" }
+
     print 'Option: '
-    gets.chomp.to_i
+
+    while (option = gets.chomp.to_i) < 1 || option > OPTIONS.size
+      puts 'Invalid option. Please try again.'
+      print 'Option: '
+    end
+
+    option
   end
 
   def run
@@ -35,12 +42,9 @@ class Main
   end
 
   def handle_option(option)
-    action = OPTIONS[option]
-    if action
-      @app.send(action)
-    else
-      puts 'Invalid option. Please try again.'
-    end
+    action = OPTIONS[option][:method] # ? get the value (method/action).
+
+    @app.send(action) if action
   end
 end
 
