2f9b44e503bd5d9a05c417a44e0684d411d15baf
ITurres <emanueliturres.co@gmail.com>
2023-11-23 20:44:01 -0300

    Linters: correct rubocop offenses on 'genre_spec.rb'

--- spec/lib_spec/genre_spec.rb
+++ spec/lib_spec/genre_spec.rb
@@ -1,36 +1,36 @@
-require_relative "../../lib/genre"
-require_relative "../../helpers/generate_id"
+require_relative '../../lib/genre'
+require_relative '../../helpers/generate_id'
 
 RSpec.describe Genre do
   subject(:genre) { Genre }
-  let(:name) { "Apple Records" }
-  let(:item) { double("Item") }
+  let(:name) { 'Apple Records' }
+  let(:item) { double('Item') }
 
   before(:each) do
-    allow_any_instance_of(genre).to receive(:generate_id).and_return("unique_id")
+    allow_any_instance_of(genre).to receive(:generate_id).and_return('unique_id')
   end
 
-  describe "#initialize" do
-    context "when the genre is initialized" do
-      it "Should create a genre instance with a title" do
+  describe '#initialize' do
+    context 'when the genre is initialized' do
+      it 'Should create a genre instance with a title' do
         genre = subject.new(name)
 
         expect(genre.name).to eq(name)
       end
 
-      it "Should create a genre instance with a title and receives an unique generated id" do
+      it 'Should create a genre instance with a title and receives an unique generated id' do
         genre = subject.new(name)
 
-        expect(genre.instance_variable_get(:@id)).to eq("unique_id")
+        expect(genre.instance_variable_get(:@id)).to eq('unique_id')
       end
     end
   end
 
-  describe "#add_item" do
-    context "when the item is not already in the genre" do
-      it "Should add an item to the genre and sets the genre for the item" do
-        genre = subject.new("My genre")
-        item = double("Item")
+  describe '#add_item' do
+    context 'when the item is not already in the genre' do
+      it 'Should add an item to the genre and sets the genre for the item' do
+        genre = subject.new('My genre')
+        item = double('Item')
 
         allow(item).to receive(:genre=)
 
